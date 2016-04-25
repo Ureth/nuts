bbf999d6480b8b1861de9ab386528266f10f4e87
Ureth <skrilax@mail.ru>
2016-04-25 19:08:38 +0300

    Reviews and main page done (without content)

--- app_client/reviews/reviews.controller.js
+++ app_client/reviews/reviews.controller.js
@@ -2,9 +2,13 @@ angular
   .module('nutsApp')
   .controller('reviewsCtrl', reviewsCtrl);
 
+
 function reviewsCtrl () {
-  var vm = this;
-  vm.pageHeader = {
-    title: 'nutsApp'
-  };
+    if(location.search.indexOf('reloaded=yes') < 0){
+        var hash = window.location.hash;
+        var loc = window.location.href.replace(hash, '');
+        loc += (loc.indexOf('?') < 0? '?' : '&') + 'reloaded=yes';
+        // SET THE ONE TIME AUTOMATIC PAGE RELOAD TIME TO 5000 MILISECONDS (5 SECONDS):
+        setTimeout(function(){window.location.href = loc + hash;}, 500);
+    }
 }
