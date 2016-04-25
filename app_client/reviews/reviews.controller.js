465d1c4d8a0595f13aa95e6c37e20cbfa8fabcfe
Ureth <skrilax@mail.ru>
2016-04-25 21:30:04 +0300

    br in scriptadded

--- app_client/reviews/reviews.controller.js
+++ app_client/reviews/reviews.controller.js
@@ -6,11 +6,14 @@ angular
 function reviewsCtrl () {
     $( document ).ready(function() {
     if(location.search.indexOf('reloaded=yes') < 0){
+
+
+
         var hash = window.location.hash;
         var loc = window.location.href.replace(hash, '');
         loc += (loc.indexOf('?') < 0? '?' : '&') + 'reloaded=yes';
         // SET THE ONE TIME AUTOMATIC PAGE RELOAD TIME TO 5000 MILISECONDS (5 SECONDS):
-        setTimeout(function(){window.location.href = loc + hash;}, 1000);
+        setTimeout(function(){window.location.href = loc + hash;}, 1500);
     	}
     });
 }
